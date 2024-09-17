3fb1144a1a9168f07244e75362ab1548e256bf4a
BPatrik <bpatrikit@gmail.com>
2024-09-17 02:34:07 +0200

    I have added the first homework of ProgAlap to the repository.
    I have added the first homework of ProgAlap
    to the repository.

---
+++ ProgAlap/Project01/Homework01/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int secondsBase;
+            int hours;
+            int minutes;
+            int seconds;
+            string output;
+            int days = 0;
+            do
+            {
+                Console.Write("Give the seconds since midnight: ");
+                int.TryParse(Console.ReadLine(), out secondsBase);
+            }
+            while (secondsBase <= 1);
+
+            if (secondsBase > 3600 *24)
+            {
+                days = secondsBase / (3600 * 24);
+                secondsBase = secondsBase - days * (3600 * 24);
+            }
+
+            if (secondsBase >= 3600)
+            {
+                hours = secondsBase / 3600;
+            }
+            else
+            {
+                hours = 0;
+            }
+            seconds = secondsBase - hours * 3600;
+            if (seconds >= 60)
+            {
+                minutes = seconds / 60;
+            }
+            else
+            {
+                minutes = 0;
+            }
+            seconds = seconds - minutes * 60;
+
+            output = hours + ":";
+            if (minutes < 10)
+            {
+                output = output + "0" + minutes + ":";
+            }
+            else
+            {
+                output = output + minutes + ":";
+            }
+            if (seconds <10)
+            {
+                output = output + "0" + seconds;
+            }
+            else
+            {
+                output = output + seconds;
+            }
+
+            Console.WriteLine("The exact time is: " + output);
+            if (days > 0)
+            {
+                Console.WriteLine("The number of days are: " + days);
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- ProgAlap/Project01/Project01/obj/Debug/net8.0/Project01.AssemblyInfo.cs
+++ ProgAlap/Project01/Project01/obj/Debug/net8.0/Project01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2204dd12370ad09197c2ee2e1ee576c6a3f02a78")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
