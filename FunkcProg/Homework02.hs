295956197360bb25f4201f72a8355389fd3ead81
BPatrik <bpatrikit@gmail.com>
2024-09-22 19:17:39 +0200

    Fixed the divides by zero error in the function.
    Fixed the divides by zero error in the function.

--- FunkcProg/Homework02.hs
+++ FunkcProg/Homework02.hs
@@ -4,10 +4,16 @@ module Hazi2 where
     addV (x1,y1) (x2,y2) = (x1+x2,y1+y2)
     subV (x1,y1) (x2,y2) = (x1-x2,y1-y2)
 
+    scaleV :: Double -> (Double,Double) -> (Double,Double)
+    scaleV s (x,y) = (s*x,s*y)
+
     scalar :: (Double,Double) -> (Double,Double) -> Double
     scalar (x1,y1) (x2,y2) = x1*x2 + y1*y2
 
     divides :: Integral a => a -> a -> Bool
+    divides 0 0 = True
+    divides 0 _ = False
+    divides _ 0 = True
     divides x y = mod y x == 0
 
     add :: (Integral a, Integral b, Num c) => a -> b -> c
