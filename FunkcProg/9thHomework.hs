f6853f5169d9402edd7d018863b0543ebd227869
BPatrikIT <bpatrikit@gmail.com>
2024-11-17 00:52:13 +0100

    Uploaaded the first part of the Haskell 9thHomework
    Uploaaded the first part of the Haskell
    9thHomework

---
+++ FunkcProg/9thHomework.hs
@@ -0,0 +1,28 @@
+module Homework where
+
+countEmptyLists :: Num a => [[[b]]] -> [a]
+countEmptyLists = map (fromIntegral . length . filter null)
+
+function1 :: (Integer -> Integer) -> Integer
+function1 g = g 13
+
+function2 :: (Integer -> Integer -> Integer) -> Integer
+function2 g = 2 * (g 25 11)
+
+function3 :: (Integer -> Integer) -> (Integer -> Integer) -> Integer
+function3 g h = g 14 + h 15
+
+function4 :: (Integer -> Integer) -> (Integer -> Integer) -> Integer
+function4 g h = g (h 1 + h 2) + g 3
+
+function5 :: (Integer -> Integer -> Integer) -> (Integer -> Integer) -> Integer
+function5 g h = g (h 1) (g 1 3) + h (g 3 5)
+
+lambda1 :: Integer
+lambda1 = function1 (\x -> x * 2)
+
+lambda2 :: Integer
+lambda2 = function2 (\x y -> 1 * x - y)
+
+lambda3 :: Integer
+lambda3 = function3 (\x -> x * 2) (\y -> y * (-1))
